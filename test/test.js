d6b3c469c6ac2ec314fd8c8395033a0fb91dba8f
Viktor Johansson <vikjoh@SE-Macbook-001s-MacBook-Pro.local>
2019-03-07 16:29:17 +0100

    Add constants and update node modules (request + mocha)

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
-var should = require("should");
-var request = require("request");
-var expect = require("chai").expect;
-var baseUrl = "https://swapi.co/api";
-var util = require("util");
+const should = require("should");
+const request = require("request");
+const expect = require("chai").expect;
+const baseUrl = "https://swapi.co/api";
+const util = require("util");
 
 describe('returns luke', function() {
     it('returns luke', function(done) {
         request.get({ url: baseUrl + '/people/1/' },
             function(error, response, body) {
-            		var bodyObj = JSON.parse(body);
+            		const bodyObj = JSON.parse(body);
             		expect(bodyObj.name).to.equal("Luke Skywalker");
             		expect(bodyObj.hair_color).to.equal("blond");
                     expect(response.statusCode).to.equal(200);
